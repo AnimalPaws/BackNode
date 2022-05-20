f53dd1b6f6301c3acab1e8ee2197280f8de96689
Ex1Kx <thenewestekigoh@gmail.com>
2022-05-20 11:51:01 -0500

    [Ex1] Added The Ports & Get Method

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Hello World')
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+  res.send('Hello World');
+})
+app.get('/Users', (req, res) =>{
+  res.send('Users Test');
+})
+app.get('/API', (req, res) =>{
+  res.json([
+    {
+      name: 'Julian',
+      age:18
+    },
+    {
+      name: 'Camilo',
+      age:19
+    }
+  ]);
+})
+
+app.get('/API/:Id', (req, res) => {
+  const { Id } = req.params;
+  res.json({
+    name: 'Julian',
+    age:18
+  });
+})
+
+app.listen(port, () =>{
+  console.log('My Port' + port);
+})
